7790db28fbf150241cbf8279b1178027757587ec
unknown <aslamsyah188@gmail.com>
2023-07-10 20:33:06 +0700

    first commit

---
+++ app/src/main/java/com/aslam202102366/login/MenuActivity.java
@@ -0,0 +1,68 @@
+package com.aslam202102366.login;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.AlertDialog;
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.AsyncHttpResponseHandler;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+import cz.msebera.android.httpclient.Header;
+
+public class MenuActivity extends AppCompatActivity {
+    private Button _tampilMahasiswaButton, _tampilForexButton, _tampilCuacaButton;
+    private Intent _tampilMahasiswaIntent, _tampilForexIntent, _tampilCuacaIntent;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_menu);
+
+        initTampilMahasiswaButton();
+        initTampilForexButton();
+    }
+
+    private void initTampilForexButton()
+    {
+        _tampilForexButton = (Button) findViewById(R.id.tampilForexButton);
+
+        _tampilForexButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v)
+            {
+                _tampilForexIntent = new Intent(getApplicationContext(), ForexMainActivity.class);
+                startActivity(_tampilForexIntent);
+
+            }
+        });
+    }
+
+    private void initTampilMahasiswaButton() {
+        _tampilMahasiswaButton = findViewById(R.id.inputMahasiswa);
+
+        _tampilMahasiswaButton.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v)
+            {
+                _tampilMahasiswaIntent = new Intent(getApplicationContext(), TampilMahasiswaActivity.class);
+                startActivity(_tampilMahasiswaIntent);
+
+            }
+        });
+
+    }
+
+}
